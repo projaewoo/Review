e7bc8e565a165b648be5fe80100de9ccb5326e33
projaewoo <62878976+projaewoo@users.noreply.github.com>
2020-08-20 11:56:59 +0900

    Update FileCopy.java

--- 0819/FileCopy.java
+++ 0819/FileCopy.java
@@ -18,7 +18,7 @@ public class FileCopy {
 				new BufferedInputStream(new FileInputStream(originalPath));		//괄호 안에 두 문장 이상일경우 ;로 구분
 				BufferedOutputStream bos =
 				new BufferedOutputStream(new FileOutputStream(targetPath))) {			//AutoCloseable의 자손이 try벗어나면 저절로 닫힘. //굳이 close()할 필요 없음.
-			
+					//읽어들일 문자가 없다면 bis.read()가 0을 반환한다고 생각할 수 있지만, 컴퓨터에게 읽어들일 문자가 없다는 뜻은 -1 음수를 반환한다는 뜻.
 			while((su = bis.read()) >= 0) {	//read()의 반환형 : int  //1byte읽어서 su에 넣음.	//BufferedInputStream이 InputSTream으로 끝나서 바이트 기준으로 읽어들임.
 				count++;		
 				bos.write(su);		//하나 읽고 내보내고
